3d8bb0062b326c11372f3eb83f5c5dcaeeb67882
Rebon402 <mkownerth777@gmail.com>
2026-06-06 13:24:09 +0700

    8.0.1

--- test_closure.rs
+++
@@ -1,6 +0,0 @@
-﻿use std::rc::Rc;
-fn needs_fn<F: Fn() -> String>(_: F) {}
-fn main() {
-    let x = Rc::new(String::from("hi"));
-    needs_fn(move || x.as_ref().clone());
-}
--- tmp_closure.rs
+++
@@ -1,6 +0,0 @@
-﻿use std::rc::Rc;
-fn main() {
-    let x = Rc::new(String::from("hi"));
-    let c = move || x.as_ref().clone();
-    let _ = c();
-}
